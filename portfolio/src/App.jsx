f0c2a847bd8e071b92aa025383e099d7ca84bf41
soumilnimbalkar26 <soumilnimblkar@gmail.com>
2023-11-24 18:51:18 +0530

    style: :tada: beginning a project
    all files are boiler template of the project

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Screen1 from "./screens/Screen1";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Screen1 />
+    </>
+  );
+}
+
+export default App;
